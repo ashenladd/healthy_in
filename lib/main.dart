9b67dbc02ff280cbbda45432a3a20628b86b211b
ashenladd <akifrh24@gmail.com>
2023-02-22 20:42:27 +0700

    Create Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:healthy_in/Pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
+      home: LoginPage(),
     );
   }
 }
