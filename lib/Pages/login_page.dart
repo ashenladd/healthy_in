514189a94a6cceb7cb9bd4214c40e7af220a604f
ashenladd <akifrh24@gmail.com>
2023-02-22 20:40:42 +0700

    Create Login Page

---
+++ lib/Pages/login_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(height: 200, child: Text("AAA")),
+    );
+  }
+}
